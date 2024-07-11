9dfd3b8c48009b2c646feb0bb024a8619863d6d3
Chris Najman <christopher.najman@gmail.com>
2024-07-11 09:31:22 +0100

    Added back to home page link on error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom"
+import { FaCircleArrowLeft } from "react-icons/fa6"
 import { useRouteError } from "react-router-dom"
 export default function Error() {
   const error = useRouteError()
 
   return (
-    <div>
-      <h1>{error.message}</h1>
-
-      <ul>
-        <li>{error.status}</li>
-        <li>{error.statusText}</li>
-      </ul>
+    <div className="content-container page-not-found-container">
+      <h1>Error: {error.message}</h1>
+      <Link
+        to="/"
+        className="back-link arrow"
+      >
+        <FaCircleArrowLeft />
+        <span>Back to the home page</span>
+      </Link>
     </div>
   )
 }
